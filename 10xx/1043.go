bd0b06a1a1ae08c7e2d72cccde800cff0d190143
wenkezhao <wenkezhao@tencent.com>
2023-04-19 16:41:31 +0800

    commit some solutions

--- 10xx/1043.go
+++ 10xx/1043.go
@@ -1,7 +1,5 @@
 package _0xx
 
-import "fmt"
-
 // 给你一个整数数组 arr，请你将该数组分隔为长度 最多 为 k 的一些（连续）子数组。分隔完成后，每个子数组的中的所有值都会变为该子数组中的最大值。
 // 返回将数组分隔变换后能够得到的元素最大和。本题所用到的测试用例会确保答案是一个 32 位整数。
 func maxSumAfterPartitioning(arr []int, k int) int {
@@ -31,8 +29,6 @@ func maxSumAfterPartitioning(arr []int, k int) int {
 		}
 		sum[i] = sm
 	}
-
-	fmt.Println(sum)
 
 	return sum[len(sum)-1]
 }
